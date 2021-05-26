6dbb8460f1cf64b5672f7a0605db955d0eb3281c
denshest <den.shest@gmail.com>
2021-05-26 14:40:40 +0300

    Fix fake api request

--- frontend/src/contexts/TocDataContext/index.tsx
+++ frontend/src/contexts/TocDataContext/index.tsx
@@ -23,7 +23,7 @@ const TocDataProvider: React.FC = (props) => {
   const [isLoading, setIsLoading] = React.useState<boolean>(true);
 
   useEffect(() => {
-    fetch('/api/v1/help_toc')
+    fetch('/api/v1/help_toc/')
       .then(response => {
         if (!response.ok) throw new Error(response.statusText);
 
